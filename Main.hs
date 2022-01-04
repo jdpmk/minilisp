7947e2e2df314718b34fa7b4f1714764cbfd161e
Joydeep Mukherjee <joydeepm02@gmail.com>
2022-01-04 15:20:35 -0500

    Add nil value to expression data type

--- Main.hs
+++ Main.hs
@@ -2,7 +2,8 @@
 
 type Symbol = String
 
-data Expr = Atom Symbol
+data Expr = Nil
+          | Atom Symbol
           | Cons Expr Expr
           deriving (Show)
 
@@ -14,10 +15,12 @@ data Expr = Atom Symbol
 --   atom
 
 car :: Expr -> Expr
+car Nil        = Nil
 car (Atom a)   = error "`car` is undefined for atomic arguments"
 car (Cons a _) = a
 
 cdr :: Expr -> Expr
+cdr Nil        = Nil
 cdr (Atom a)   = error "`cdr` is undefined for atomic arguments"
 cdr (Cons _ b) = b
 
@@ -25,10 +28,13 @@ cons :: Expr -> Expr -> Expr
 cons = Cons
 
 eq :: Expr -> Expr -> Bool
+eq Nil        Nil        = True
 eq (Atom a)   (Atom b)   = a == b
 eq (Cons _ _) _          = error "`eq` is undefined for non-atomic arguments"
 eq _          (Cons _ _) = error "`eq` is undefined for non-atomic arguments"
+eq _          _          = False
 
 atom :: Expr -> Bool
+atom Nil        = True
 atom (Atom _)   = True
 atom (Cons _ _) = False
