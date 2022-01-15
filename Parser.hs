829824139100967fe84c7ac144140a1df7dbf4f9
Joydeep Mukherjee <joydeepm02@gmail.com>
2022-01-15 12:01:45 -0800

    Implement Functor, Applicative, Monad for Parser type

--- Parser.hs
+++ Parser.hs
@@ -1,3 +1,42 @@
 module Parser where
 
--- TODO: Implement parser
+data Error i = EOF
+             | Unexpected i
+             | Empty
+             deriving (Show, Eq)
+
+newtype Parser i a = Parser
+  { runParser :: [i] -> Either [Error i] (a, [i])
+  }
+
+instance Functor (Parser i) where
+  fmap f (Parser parse) = Parser $ \input ->
+    case parse input of
+      Left error    -> Left error
+      Right (x, xs) -> Right (f x, xs)
+
+instance Applicative (Parser i) where
+  pure x = Parser $ \input -> Right (x, input)
+
+  Parser f <*> Parser p = Parser $ \input -> do
+    (f', xs) <- f input
+    (x, xs') <- p xs
+    pure (f' x, xs')
+
+instance Monad (Parser i) where
+  return = pure
+
+  (Parser p) >>= f = Parser $ \input -> do
+    (x, xs) <- p input
+    runParser (f x) xs
+
+satisfy :: (i -> Bool) -> Parser i i
+satisfy pred = Parser $ \input ->
+  case input of
+    [] -> Left [EOF]
+    x:xs
+      | pred x    -> Right (x, xs)
+      | otherwise -> Left [Unexpected x]
+
+char :: Eq a => a -> Parser a a
+char c = satisfy (== c)
