9872c017a20874b7b5c12e8450c2cbfe2f406fc0
Joydeep Mukherjee <joydeepm02@gmail.com>
2022-01-17 16:36:24 -0800

    Refactor Atom and Expr definitions

--- Lisp.hs
+++ Lisp.hs
@@ -1,104 +1,82 @@
 module Lisp where
 
-data Symbol = Identifier String
-            | String String
-            | Integer Int
-            deriving (Show, Eq)
-
-data Expr = Nil
+data Atom = Nil
           | T
-          | Atom Symbol
-          | Function Function
-          | Cons Expr Expr
+          | Identifier String
+          | String String
+          | Integer Int
           deriving (Show, Eq)
 
-data Function = CAR
-              | CDR
-              | CONS
-              | EQQ
-              | ATOM
-              | LAMBDA
-              | LABEL
-              | QUOTE
-              | COND
-              deriving (Show, Eq)
-
-car :: Expr -> Expr
-car Nil          = Nil
-car T            = error "`car` is undefined for atomic arguments"
-car (Atom _)     = error "`car` is undefined for atomic arguments"
-car (Function _) = error "`car` is undefined for atomic arguments"
-car (Cons a _)   = a
-
-cdr :: Expr -> Expr
-cdr Nil          = Nil
-cdr T            = error "`cdr` is undefined for atomic arguments"
-cdr (Atom _)     = error "`cdr` is undefined for atomic arguments"
-cdr (Function _) = error "`cdr` is undefined for atomic arguments"
-cdr (Cons _ b)   = b
+data Expr = Atom Atom
+          | Cons Expr Expr
+          deriving (Show, Eq)
 
 cons :: Expr -> Expr -> Expr
 cons = Cons
 
+car :: Expr -> Expr
+car (Atom _)   = error "`car` is undefined for atomic arguments"
+car (Cons a _) = a
+
+cdr :: Expr -> Expr
+cdr (Atom _)   = error "`cdr` is undefined for atomic arguments"
+cdr (Cons _ b) = b
+
 eq :: Expr -> Expr -> Expr
-eq a b = if a == b then T else Nil
+eq (Atom a) (Atom b) = if a == b then Atom T else Atom Nil
+eq _        _        = Atom Nil
 
 atom :: Expr -> Expr
-atom (Cons _ _) = Nil
-atom _          = T
+atom (Atom _) = Atom T
+atom _        = Atom Nil
 
 null' :: Expr -> Expr
-null' Nil = T
-null' _   = Nil
+null' (Atom Nil) = Atom T
+null' _          = Atom Nil
 
 pairlis :: Expr -> Expr -> Expr -> Expr
 pairlis x y a
-  | null' x == T = a
-  | otherwise    = cons (cons (car x) (car y)) (pairlis (cdr x) (cdr y) a)
+  | null' x == Atom T = a
+  | otherwise         = cons (cons (car x) (car y)) (pairlis (cdr x) (cdr y) a)
 
 assoc :: Expr -> Expr -> Expr
 assoc x a
-  | eq (car $ car a) x == T = car a
-  | otherwise               = assoc x (cdr a)
+  | eq (car $ car a) x == Atom T = car a
+  | otherwise                    = assoc x (cdr a)
 
 evcon :: Expr -> Expr -> Expr
 evcon c a
-  | null' (eval (car $ car c) a) == Nil = eval (car $ cdr $ car c) a
-  | otherwise                           = evcon (cdr c) a
+  | null' (eval (car $ car c) a) == Atom T = eval (car $ cdr $ car c) a
+  | otherwise                              = evcon (cdr c) a
 
 evlis :: Expr -> Expr -> Expr
 evlis m a
-  | null' m == T = Nil
-  | otherwise    = cons (eval (car m) a) (evlis (cdr m) a)
+  | null' m == Atom T = Atom Nil
+  | otherwise         = cons (eval (car m) a) (evlis (cdr m) a)
 
 apply :: Expr -> Expr -> Expr -> Expr
 apply fn x a
-  | atom fn == T =
+  | atom fn == Atom T =
       case fn of
-        (Function CAR)  -> car $ car x
-        (Function CDR)  -> cdr $ car x
-        (Function CONS) -> cons (car x) (car $ cdr x)
-        (Function ATOM) -> atom (car x)
-        (Function EQQ)  -> eq (car x) (car $ cdr x)
-        _               -> apply (eval fn a) x a
-  | eq (car fn) (Function LAMBDA) == T = eval (car $ cdr $ cdr fn) (pairlis (car $ cdr fn) x a)
-  | eq (car fn) (Function LABEL)  == T = apply (car $ cdr $ cdr fn) x (cons (cons (car $ cdr fn) (car $ cdr $ cdr fn)) a)
+        (Atom (Identifier "car"))  -> car $ car x
+        (Atom (Identifier "cdr"))  -> cdr $ car x
+        (Atom (Identifier "cons")) -> cons (car x) (car $ cdr x)
+        (Atom (Identifier "atom")) -> atom (car x)
+        (Atom (Identifier "eq"))  -> eq (car x) (car $ cdr x)
+        _                          -> apply (eval fn a) x a
+  | eq (car fn) (Atom (Identifier "lambda")) == Atom T =
+      eval (car $ cdr $ cdr fn) (pairlis (car $ cdr fn) x a)
+  | eq (car fn) (Atom (Identifier "label"))  == Atom T =
+      apply (car $ cdr $ cdr fn) x (cons (cons (car $ cdr fn) (car $ cdr $ cdr fn)) a)
 
 eval :: Expr -> Expr -> Expr
 eval e a
-  | atom e       == T = cdr (assoc e a)
-  | atom (car e) == T =
+  | atom e       == Atom T = cdr (assoc e a)
+  | atom (car e) == Atom T =
       case car e of
-        (Function QUOTE) -> car $ cdr e
-        (Function COND)  -> evcon (cdr e) a
-        _                -> apply (car e) (evlis (cdr e) a) a
-  | otherwise         = apply (car e) (evlis (cdr e) a) a
+        (Atom (Identifier "quote")) -> car $ cdr e
+        (Atom (Identifier "cond"))  -> evcon (cdr e) a
+        _                           -> apply (car e) (evlis (cdr e) a) a
 
 evalquote :: Expr -> Expr -> Expr
-evalquote fn x = apply fn x Nil
-
--- Test Cases
---   To run, paste `Input` expression into REPL. Match output with `Result` expression.
-
--- Input: evalquote (Cons (Function LAMBDA) (Cons (Cons (Atom (Identifier "X")) (Cons (Atom (Identifier "Y")) Nil)) (Cons (Cons (Function CONS) (Cons (Cons (Function CAR) (Cons (Atom (Identifier "X")) Nil)) (Cons (Atom (Identifier "Y")) Nil))) Nil))) (Cons (Cons (Atom (Identifier "A")) (Cons (Atom (Identifier "B")) Nil)) (Cons (Cons (Atom (Identifier "C")) (Cons (Atom (Identifier "D")) Nil)) Nil))
--- Result: Cons (Atom (Identifier "A")) (Cons (Atom (Identifier "C")) (Cons (Atom (Identifier "D")) Nil))
+evalquote fn x = apply fn x (Atom Nil)
